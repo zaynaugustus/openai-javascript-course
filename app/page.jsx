793cfec8ae1bbbd62bd5efc29a9819e9cdf8f2bd
zaynaugustus <133876614+zaynaugustus@users.noreply.github.com>
2023-07-02 11:22:20 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -10,7 +10,23 @@ export default function Home() {
       </h1>
       <div className="flex flex-row justify-start">
         <div className="flex flex-col items-start justify-center min-h-screen text-gray-800 py-4 px-4 sm:px-6 lg:px-8 w-6/12">
-          
+                    <h2
+            className={`w-full text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl text-left ${pressStart2P.className}`}
+          >
+            Chat Anything
+          </h2>
+          <p
+            className={`w-full mt-6 max-w-2xl text-center text-lg leading-7 sm:text-2xl sm:leading-9 sm:text-left lg:text-3xl ${instrumentSans.className}`}
+          >
+            <ul className="list-none">
+              <li>PDF-Chat</li>
+              <li>Video-Chat</li>
+              <li>Content Wizard</li>
+              <li>RoboHR</li>
+              <li>Stream - Rap Battle</li>
+              <li>Memory</li>
+            </ul>
+          </p>
         </div>
         {/* Gallery */}
         <Gallery />
