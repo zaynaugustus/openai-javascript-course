7e2e04990e636ac2ed4c0a9fb1c6ba305d01d90a
zaynaugustus <133876614+zaynaugustus@users.noreply.github.com>
2023-07-02 11:56:04 +0530

    Update pdf/page.jsx

--- app/pdf/page.jsx
+++ app/pdf/page.jsx
@@ -109,8 +109,7 @@ const PDFLoader = () => {
               boldText="How to get rich? How to be happy?"
               description="This tool will
             let you ask anything contained in a PDF document. This tool uses
-            Embeddings, Pinecone, VectorDBQAChain, and VectorStoreAgents. Head over to Module 1 to
-            get started!"
+            Embeddings, Pinecone, VectorDBQAChain, and VectorStoreAgents."
             />
             <ButtonContainer>
               {/* <Button
